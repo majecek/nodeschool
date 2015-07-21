4a7f949d2485c681a4ea189e89c551b22661d197
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-07-21 11:05:55 +0200

    last exercise

--- learnyounode/2015/12.js
+++ learnyounode/2015/12.js
@@ -1,15 +1,33 @@
 var http = require('http')
-var fs = require('fs')
-var map = require('through2-map')
+var url = require('url')
 
 
-console.log(process.argv)
+//console.log(process.argv)
 
-var server = http.createServer(function (request, response) {
-    //response.writeHead(200, {'contenty-type': 'text/plain'})
-    request.pipe(map(function (chunk) {
-        return chunk.toString().toUpperCase()
-    })).pipe(response)
+var server = http.createServer(function (req, res) {
+    if (req.method != 'GET') {
+        return res.end("Sorry, you have to use GET")
+    }
+
+
+    var q = url.parse(req.url, true)
+    console.log(q)
+
+    res.writeHead(200, {'contenty-type': 'text/json'})
+
+    var now = new Date(q.query.iso)
+    console.log(now)
+    var jsonDate = JSON.stringify({hour: now.getHours(), minute: now.getMinutes(), second: now.getSeconds()}) //,,)
+    var jsonUnixDate = JSON.stringify({unixtime: now.getTime()})
+
+    console.log(jsonDate)
+    console.log(jsonUnixDate)
+
+    if (q.pathname === "/api/parsetime") {
+        return res.end(jsonDate)
+    } else {
+        return res.end (jsonUnixDate)
+    }
 })
 
 server.listen(process.argv[2]);
