6620037abef07132da83ced682d8f113d237fa1f
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-07-22 11:44:32 +0200

    react #1

---
+++ learnyoureact/2015/program.js
@@ -0,0 +1,19 @@
+/**
+ * Created by marek on 22/07/15.
+ */
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+require('node-jsx').install();
+
+app.use('/', function(req, res) {
+    res.render('index', '');
+});
+
+app.listen(app.get('port'), function() {});
