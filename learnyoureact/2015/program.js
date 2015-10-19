cae2a91ec71e5c0a344273e740f0c101688acbb7
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-10-19 13:42:50 +0200

    react your way

--- learnyoureact/2015/program.js
+++ learnyoureact/2015/program.js
@@ -3,6 +3,14 @@
  * Created by marek on 22/07/15.
  */
 
+var React = require('react');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+
 var express = require('express');
 var app = express();
 
@@ -12,10 +20,12 @@ app.set('views', __dirname + '/views');
 app.engine('jsx', require('express-react-views').createEngine());
 
 require('node-jsx').install();
+var TodoBox = require('./views/index.jsx');
 
-var data = [{title: 'Shopping', detail: process.argv[3]},
+var data = [
+    {title: 'Shopping', detail: process.argv[3]},
     {title: 'Hair cut', detail: process.argv[4]}
-            ];
+];
 app.use('/', function(req, res) {
     res.render('index', {data: data});
 });
